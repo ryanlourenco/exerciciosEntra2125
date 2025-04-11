cd90b7acd2c0b1f694f3484ebb46a1bcfa14f9f8
RYAN ANTONIO LOURENÇO <ryan.lourenco@alunos.sc.senac.br>
2025-04-11 19:30:56 -0300

    exercicio01

---
+++ exercicio/src/exercicio01.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class exercicio01 {
+    public static void main(String[] args) {
+// exercicio 01
+        int N = 20;
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
