b82520db0e42880c9705d26d1e6b5ce54e9c9b36
RYAN ANTONIO LOURENÇO <ryan.lourenco@alunos.sc.senac.br>
2025-04-11 19:40:31 -0300

    exercicio02

---
+++ exercicio/src/exercicio02.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class exercicio02 {
+    public static void main(String[] args) {
+// exercicio 01
+//        int N = 20;
+//        for (int i = 1; i <= N; i++) {
+//            System.out.println(i);
+//        }
+
+        //Questão 02 - Soma de 1 a N
+        int N = 5;
+        int soma = 0;
+        for (int i = 1; i <= N; i++) {
+            soma += i;
+
+        }
+
+        }
+}
