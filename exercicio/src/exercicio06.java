0e93c0911d393b89e9de4a0396d2ff10ec620cb4
RYAN ANTONIO LOURENÇO <ryan.lourenco@alunos.sc.senac.br>
2025-04-11 19:59:27 -0300

    exercicio06

--- exercicio/src/exercicio06.java
+++ exercicio/src/exercicio06.java
@@ -1,11 +1,14 @@
-import java.util.ArrayList;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class exercicio05 {
+public class exercicio06 {
     public static void main(String[] args) {
 
-        
+        //Questão 06 - Fatorial de um numero
+        int fatorial = 5;
+        for (int i = 4; i > 0; i--) {
+            fatorial *= i;
+        }
+
 
 
 
