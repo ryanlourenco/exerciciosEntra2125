f6362d91266a61b2cd45f459e7663b8ddc7fd67e
RYAN ANTONIO LOURENÇO <ryan.lourenco@alunos.sc.senac.br>
2025-04-11 20:15:07 -0300

    exercicio17

---
+++ exercicio/src/exercicio17.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class exercicio17 {
+    public static void main(String[] args) {
+
+
+
+
+        //Questão 17 - Inverter uma lista
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(6);
+
+        Collections.reverse(numeros);
+        System.out.println(numeros);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
